b9bed9368984d469a1283a203136cb7f04eabbd3
Roman <123@gmail.com>
2024-02-10 11:43:17 +0200

    ver 1.0

---
+++ main.py
@@ -0,0 +1,51 @@
+# class Grandparent():
+#     height = 170
+#     satiety = 100
+#     age = 60
+#
+# class Parent(Grandparent):
+#     age = 40
+#
+# class Child(Parent):
+#     height = 50
+#     age = 10
+#     def __init__(self):
+#         print(self.height)
+#         print(self.satiety)
+#         print(self.age)
+#
+# ch = Child()
+
+class Hello_world:
+    hello = "Hello" # public
+    _hello = "_Hello" # protected
+    __hello = "__Hello" # private
+
+    def __init__(self):
+        self.world = "World"
+        self._world = "_World"
+        self.__world = "__World"
+
+    def printer(self):
+        print(self.hello)
+        print(self._hello)
+        print(self.__hello)
+
+        print(self.world)
+        print(self._world)
+        print(self.__world)
+
+class Hi(Hello_world):
+    def hi_printer(self):
+        print(self.hello)
+        print(self._hello)
+        # print(self.__hello)
+
+        print(self.world)
+        print(self._world)
+        # print(self.__world)
+
+h = Hello_world()
+h.printer()
+hi = Hi()
+hi.hi_printer()
